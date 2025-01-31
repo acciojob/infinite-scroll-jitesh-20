a2495d4f9c6dca60f2e85e6e4529e62930fc5cae
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-31 11:36:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-//your code here!
+const list = document.getElementById('infi-list');
 
+function addItems() {
+    for (let i = 0; i < 2; i++) {
+        const li = document.createElement('li');
+        li.textContent = `Item ${list.children.length + 1}`;
+        list.appendChild(li);
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+    addItems();
+}
+
+document.querySelector('.container').addEventListener('scroll', function() {
+    const container = this;
+    if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+        addItems();  
+    }
+});
